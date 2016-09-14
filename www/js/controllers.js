0db6d55a783ee012f605170a1642ec839edbfa01
Abhinav <thakareabhinav@gmail.com>
2016-09-14 14:16:16 +0530

    Added Ionic material and removed unwanted code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,113 +1,70 @@
-angular.module('starter.controllers', ['ionic'])
+angular.module('starter').controller('DashCtrl', function($scope) {
 
-.controller('DashCtrl', function($scope) {})
+    }).controller('ChatsCtrl', function($scope, Chats) {
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+        $scope.chats = Chats.all();
+        $scope.remove = function(chat) {
+            Chats.remove(chat);
+        };
+    }).controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+        $scope.chat = Chats.get($stateParams.chatId);
+    }).controller('Messages', function($scope, $timeout, $ionicScrollDelegate) {
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+        $scope.hideTime = true;
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+        var alternate,
+            isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
 
-.directive('input', function($timeout) {
-  return {
-    restrict: 'E',
-    scope: {
-      'returnClose': '=',
-      'onReturn': '&',
-      'onFocus': '&',
-      'onBlur': '&'
-    },
-    link: function(scope, element, attr) {
-      element.bind('focus', function(e) {
-        if (scope.onFocus) {
-          $timeout(function() {
-            scope.onFocus();
-          });
-        }
-      });
-      element.bind('blur', function(e) {
-        if (scope.onBlur) {
-          $timeout(function() {
-            scope.onBlur();
-          });
-        }
-      });
-      element.bind('keydown', function(e) {
-        if (e.which == 13) {
-          if (scope.returnClose) element[0].blur();
-          if (scope.onReturn) {
-            $timeout(function() {
-              scope.onReturn();
+        $scope.sendMessage = function() {
+            alternate = !alternate;
+
+            var d = new Date();
+            d = d.toLocaleTimeString().replace(/:\d+ /, ' ');
+
+            $scope.messages.push({
+                userId: alternate ? '12345' : '54321',
+                text: $scope.data.message,
+                time: d
             });
-          }
-        }
-      });
-    }
-  }
-})
-.controller('Messages', function($scope, $timeout, $ionicScrollDelegate) {
 
-  $scope.hideTime = true;
+            delete $scope.data.message;
+            $ionicScrollDelegate.scrollBottom(true);
 
-  var alternate,
-    isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
+        };
 
-  $scope.sendMessage = function() {
-    alternate = !alternate;
 
-    var d = new Date();
-  d = d.toLocaleTimeString().replace(/:\d+ /, ' ');
+        $scope.inputUp = function() {
+            if (isIOS) $scope.data.keyboardHeight = 216;
+            $timeout(function() {
+                $ionicScrollDelegate.scrollBottom(true);
+            }, 300);
 
-    $scope.messages.push({
-      userId: alternate ? '12345' : '54321',
-      text: $scope.data.message,
-      time: d
+        };
+
+        $scope.inputDown = function() {
+            if (isIOS) $scope.data.keyboardHeight = 0;
+            $ionicScrollDelegate.resize();
+        };
+
+        $scope.closeKeyboard = function() {
+            // cordova.plugins.Keyboard.close();
+        };
+
+
+        $scope.data = {};
+        $scope.myId = '12345';
+        $scope.messages = [];
+
+    })
+    .controller('AccountCtrl', function($scope) {
+        $scope.settings = {
+            enableFriends: true
+        };
     });
-
-    delete $scope.data.message;
-    $ionicScrollDelegate.scrollBottom(true);
-
-  };
-
-
-  $scope.inputUp = function() {
-    if (isIOS) $scope.data.keyboardHeight = 216;
-    $timeout(function() {
-      $ionicScrollDelegate.scrollBottom(true);
-    }, 300);
-
-  };
-
-  $scope.inputDown = function() {
-    if (isIOS) $scope.data.keyboardHeight = 0;
-    $ionicScrollDelegate.resize();
-  };
-
-  $scope.closeKeyboard = function() {
-    // cordova.plugins.Keyboard.close();
-  };
-
-
-  $scope.data = {};
-  $scope.myId = '12345';
-  $scope.messages = [];
-
-})
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
