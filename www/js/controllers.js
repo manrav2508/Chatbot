792a9e349bd7a3eb68ca8d89f7ac7d656ad1760c
Abhinav <thakareabhinav@gmail.com>
2016-09-14 22:23:48 +0530

    Added motion effects

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,94 +1,166 @@
-angular.module('starter').controller('DashCtrl', function($scope, $state) {
-		 $scope.logout = function(chat) {
-	     	$state.go('login', {}, {
-	             reload: true
-	         });
-	     };
-    }).controller('ChatsCtrl', function($scope, Chats, $state) {
-        // With the new view caching in Ionic, Controllers are only called
-        // when they are recreated or on app start, instead of every page change.
-        // To listen for when this page is active (for example, to refresh data),
-        // listen for the $ionicView.enter event:
-        //
-        //$scope.$on('$ionicView.enter', function(e) {
-        //});
-
-        $scope.chats = Chats.all();
-        $scope.remove = function(chat) {
-            Chats.remove(chat);
-        };
-        $scope.logout = function(chat) {
-        	$state.go('login', {}, {
-                reload: true
-            });
-        };
-    }).controller('loginCtrl', function($scope, $state) {
-        $scope.data = {};
-        $scope.login = function(data) {
-        	if(data.username ==='Abhinav' && data.password ==='123123'){
-        		$state.go('tab.dash', {}, {
-                    reload: true
-                });
-        	}
-        };
-    }).controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-        $scope.chat = Chats.get($stateParams.chatId);
-    }).controller('Messages', function($scope, $timeout, $ionicScrollDelegate) {
-
-        $scope.hideTime = true;
-
-        var alternate,
-            isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
-
-        $scope.sendMessage = function() {
-            alternate = !alternate;
-
-            var d = new Date();
-            d = d.toLocaleTimeString().replace(/:\d+ /, ' ');
-
-            $scope.messages.push({
-                userId: alternate ? '12345' : '54321',
-                text: $scope.data.message,
-                time: d
-            });
-
-            delete $scope.data.message;
+/* global angular, document, window */
+'use strict';
+angular.module('rbbr').controller('AppCtrl', function($scope, $ionicModal, $ionicPopover, $timeout) {
+    // Form data for the login modal
+    $scope.loginData = {};
+    $scope.isExpanded = false;
+    $scope.hasHeaderFabLeft = false;
+    $scope.hasHeaderFabRight = false;
+    var navIcons = document.getElementsByClassName('ion-navicon');
+    for (var i = 0; i < navIcons.length; i++) {
+        navIcons.addEventListener('click', function() {
+            this.classList.toggle('active');
+        });
+    }
+    $scope.hideNavBar = function() {
+        document.getElementsByTagName('ion-nav-bar')[0].style.display = 'none';
+    };
+    $scope.showNavBar = function() {
+        document.getElementsByTagName('ion-nav-bar')[0].style.display = 'block';
+    };
+    $scope.noHeader = function() {
+        var content = document.getElementsByTagName('ion-content');
+        for (var i = 0; i < content.length; i++) {
+            if (content[i].classList.contains('has-header')) {
+                content[i].classList.toggle('has-header');
+            }
+        }
+    };
+    $scope.setExpanded = function(bool) {
+        $scope.isExpanded = bool;
+    };
+    $scope.setHeaderFab = function(location) {
+        var hasHeaderFabLeft = false;
+        var hasHeaderFabRight = false;
+        switch (location) {
+            case 'left':
+                hasHeaderFabLeft = true;
+                break;
+            case 'right':
+                hasHeaderFabRight = true;
+                break;
+        }
+        $scope.hasHeaderFabLeft = hasHeaderFabLeft;
+        $scope.hasHeaderFabRight = hasHeaderFabRight;
+    };
+    $scope.hasHeader = function() {
+        var content = document.getElementsByTagName('ion-content');
+        for (var i = 0; i < content.length; i++) {
+            if (!content[i].classList.contains('has-header')) {
+                content[i].classList.toggle('has-header');
+            }
+        }
+    };
+    $scope.hideHeader = function() {
+        $scope.hideNavBar();
+        $scope.noHeader();
+    };
+    $scope.showHeader = function() {
+        $scope.showNavBar();
+        $scope.hasHeader();
+    };
+    $scope.clearFabs = function() {
+        var fabs = document.getElementsByClassName('button-fab');
+        if (fabs.length && fabs.length > 1) {
+            fabs[0].remove();
+        }
+    };
+}).controller('LoginCtrl', function($scope, $timeout, $stateParams, ionicMaterialInk) {
+    $scope.$parent.clearFabs();
+    $timeout(function() {
+        $scope.$parent.hideHeader();
+    }, 0);
+    ionicMaterialInk.displayEffect();
+}).controller('ChatCtrl', function($scope, $stateParams, $timeout, ionicMaterialInk, ionicMaterialMotion, $ionicScrollDelegate) {
+    // Set Header
+    $scope.$parent.showHeader();
+    $scope.$parent.clearFabs();
+    $scope.$parent.setHeaderFab('left');
+    // Delay expansion
+    $timeout(function() {
+        $scope.isExpanded = true;
+        $scope.$parent.setExpanded(true);
+    }, 300);
+    // Set Motion
+    ionicMaterialMotion.fadeSlideInRight();
+    // Set Ink
+    ionicMaterialInk.displayEffect();
+    $scope.hideTime = true;
+    var alternate,
+        isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
+    $scope.sendMessage = function() {
+        alternate = !alternate;
+        var d = new Date();
+        d = d.toLocaleTimeString().replace(/:\d+ /, ' ');
+        $scope.messages.push({
+            userId: alternate ? '12345' : '54321',
+            text: $scope.data.message,
+            time: d
+        });
+        delete $scope.data.message;
+        $ionicScrollDelegate.scrollBottom(true);
+    };
+    $scope.inputUp = function() {
+        if (isIOS) $scope.data.keyboardHeight = 216;
+        $timeout(function() {
             $ionicScrollDelegate.scrollBottom(true);
-
-        };
-
-
-        $scope.inputUp = function() {
-            if (isIOS) $scope.data.keyboardHeight = 216;
-            $timeout(function() {
-                $ionicScrollDelegate.scrollBottom(true);
-            }, 300);
-
-        };
-
-        $scope.inputDown = function() {
-            if (isIOS) $scope.data.keyboardHeight = 0;
-            $ionicScrollDelegate.resize();
-        };
-
-        $scope.closeKeyboard = function() {
-            // cordova.plugins.Keyboard.close();
-        };
-
-
-        $scope.data = {};
-        $scope.myId = '12345';
-        $scope.messages = [];
-
-    })
-    .controller('AccountCtrl', function($scope,$state) {
-        $scope.settings = {
-            enableFriends: true
-        };
-        $scope.logout = function(chat) {
-        	$state.go('login', {}, {
-                reload: true
-            });
-        };
-        
+        }, 300);
+    };
+    $scope.inputDown = function() {
+        if (isIOS) $scope.data.keyboardHeight = 0;
+        $ionicScrollDelegate.resize();
+    };
+    $scope.closeKeyboard = function() {
+        // cordova.plugins.Keyboard.close();
+    };
+    $scope.data = {};
+    $scope.myId = '12345';
+    $scope.messages = [];
+}).controller('ProfileCtrl', function($scope, $stateParams, $timeout, ionicMaterialMotion, ionicMaterialInk) {
+    // Set Header
+    $scope.$parent.showHeader();
+    $scope.$parent.clearFabs();
+    $scope.isExpanded = false;
+    $scope.$parent.setExpanded(false);
+    $scope.$parent.setHeaderFab(false);
+    // Set Motion
+    $timeout(function() {
+        ionicMaterialMotion.slideUp({
+            selector: '.slide-up'
+        });
+    }, 300);
+    $timeout(function() {
+        ionicMaterialMotion.fadeSlideInRight({
+            startVelocity: 3000
+        });
+    }, 700);
+    // Set Ink
+    ionicMaterialInk.displayEffect();
+}).controller('ActivityCtrl', function($scope, $stateParams, $timeout, ionicMaterialMotion, ionicMaterialInk) {
+    $scope.$parent.showHeader();
+    $scope.$parent.clearFabs();
+    $scope.isExpanded = true;
+    $scope.$parent.setExpanded(true);
+    $scope.$parent.setHeaderFab('right');
+    $timeout(function() {
+        ionicMaterialMotion.fadeSlideIn({
+            selector: '.animate-fade-slide-in .item'
+        });
+    }, 200);
+    // Activate ink for controller
+    ionicMaterialInk.displayEffect();
+}).controller('GalleryCtrl', function($scope, $stateParams, $timeout, ionicMaterialInk, ionicMaterialMotion) {
+    $scope.$parent.showHeader();
+    $scope.$parent.clearFabs();
+    $scope.isExpanded = true;
+    $scope.$parent.setExpanded(true);
+    $scope.$parent.setHeaderFab(false);
+    // Activate ink for controller
+    ionicMaterialInk.displayEffect();
+    ionicMaterialMotion.pushDown({
+        selector: '.push-down'
     });
+    ionicMaterialMotion.fadeSlideInRight({
+        selector: '.animate-fade-slide-in .item'
+    });
+});
